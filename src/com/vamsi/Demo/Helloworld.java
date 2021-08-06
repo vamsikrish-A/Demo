4317eeaf7efc43dfe562c08d1edb0966a48f7e4d
temp <krishnavamsi165@gmail.com>
2021-08-06 09:57:15 +0530

    uc2 completed

--- src/com/vamsi/Demo/Helloworld.java
+++ src/com/vamsi/Demo/Helloworld.java
@@ -4,5 +4,6 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("uc1 completed");
+        System.out.println("uc2 completed");
     }
 }
